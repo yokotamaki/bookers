08af2ad107c6140ce070bfa262d6e2f32ac39fb4
yokotamaki <ymaki0626@gmail.com>
2020-12-10 03:52:03 +0000

    編集ページ、詳細ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,14 +11,22 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
   end  
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end  
   
   private
   def book_params
